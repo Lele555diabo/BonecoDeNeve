09234535bba446d75418be6046abea09fd1c181a
Lele555diabo <gustavo.miglioretto@escola.pr.gov.br>
2024-09-23 15:30:48 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+let cor
+
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background("lightblue");
+  fill("white");
+  strokeWeight(1)
+  stroke("white");
+  ellipse(200, 320, 210, 140);
+  ellipse(200, 230, 160, 110);
+  ellipse(200, 150, 100, 100);
+  fill("black");
+  circle(185, 133, 20, 20);
+  circle(215, 133, 20, 20);
+  fill("white");
+  circle(180, 126, 5, 5);
+  circle(190, 140, 5, 5);
+  
+  circle(220, 140, 5, 5);
+  circle(210, 126, 5, 5);
+  fill("orange");
+  triangle(200, 150, 200, 140, 220, 170);
+  fill("black");
+    circle(200, 170, 7, 7);
+  circle(210, 169, 7, 7);
+  circle(220, 167, 7, 7);
+  circle(190, 169, 7, 7);
+  circle(180, 167, 7, 7);
+  
+  fill("rgba(255,182,193,0.63)");
+  ellipse(175, 154, 20, 8);
+  ellipse(225, 154, 20, 8);
+  
+  fill("black")
+  circle(200, 250, 7, 7);
+  circle(200, 230, 7, 7);
+  circle(200, 210, 7, 7);
+  
+  stroke("brown");
+  strokeWeight(5);
+  line(120, 240, 80, 170);
+  line(280, 240, 300, 300);
+  
+    stroke("black");
+  strokeWeight(5);
+  line(250, 380, 250, 400);
+  line(150, 380, 150, 400);
+  
+  
+
+
+}
